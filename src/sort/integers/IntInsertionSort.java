9b15c901ec26c8da57777ff6736ce8eacb79f5b5
Richmond Chang <changrichmond@gmail.com>
2015-04-01 23:44:45 -0700

    Implementation of insertion sort for integers straight from google doc:     - Forgot the package

---
+++ src/sort/integers/IntInsertionSort.java
@@ -0,0 +1,53 @@
+package sort.integers;
+
+public class IntInsertionSort extends IntISort 
+{
+    @Override
+public int[] sort(int[] list)
+{
+for(int i = 1; i < list.length; i++)
+{
+for(int j = i; j >= 0 && list[j] > list[i]; j--)
+{
+    swap(list, i, j);
+}
+}
+return list;
+}
+
+@Override
+public int[][] sortSteps(int[] list)
+{
+    int[][] res = new int[10];
+    res[0] = list;
+
+    for(int i = 1; i < list.length; i++)
+    {
+        for(int j = i; j >= 0 && list[j] > list[i]; j--)
+        {
+            swap(list, i, j);
+        }
+        res[i] =  list.clone(); // only for primitives
+    }
+    return list;
+}
+            
+@Override
+public String name()
+{
+    return "Selection Sort";
+}
+
+private boolean swap(int[] list, int a, int b)
+{
+    if(a >= 0 && a < list.length && b >= 0 && b < list.length)
+    {
+        int temp = list[a];
+        list[a] = list[b];
+        list[b] = list[a];
+        return true;
+    }
+    return false;
+}
+}
+        
